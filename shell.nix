44779005a7a45adf811afd77d987422926b10d40
Brian Hicks <brian@brianthicks.com>
2019-09-06 15:55:24 -0500

    init

---
+++ shell.nix
@@ -0,0 +1,12 @@
+with import (builtins.fetchTarball rec {
+  # grab a hash from here: https://nixos.org/channels/
+  name = "nixpkgs-darwin-18.09pre154171.2aead2422f8";
+  url = "https://github.com/nixos/nixpkgs/archive/2aead2422f85562a09619a657ecab0ca405f2d72.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "0hg7q8ggpwni0gswa3a74nnn5pa3gi5xw29735l7w59dw9nln8lp";
+}) { };
+
+stdenv.mkDerivation {
+  name = "elm-swapper";
+  buildInputs = [ git bash ];
+}
