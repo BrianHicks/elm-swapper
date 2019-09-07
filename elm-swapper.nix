6bb6ee6cf435bb732fe33ab74b155b0098d55d81
Brian Hicks <brian@brianthicks.com>
2019-09-07 14:48:13 -0500

    get the nearest elm.json

--- elm-swapper.nix
+++ elm-swapper.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, directory, stdenv }:
+{ mkDerivation, base, directory, filepath, stdenv }:
 mkDerivation {
   pname = "elm-swapper";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base directory ];
+  executableHaskellDepends = [ base directory filepath ];
   description = "automatically dispatch to the right version of Elm";
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,27 @@
 module Main where
 
 import System.Directory
+import System.FilePath
 
+nearestElmJson :: IO (Maybe FilePath)
+nearestElmJson =
+  getCurrentDirectory >>= nearestElmJsonHelp
+
+nearestElmJsonHelp :: FilePath -> IO (Maybe FilePath)
+nearestElmJsonHelp filepath = do
+  let candidate = filepath </> "elm.json"
+  exists <- doesFileExist candidate
+  if exists then
+    pure $ Just candidate
+  else
+    let next = takeDirectory filepath in
+
+    if next == filepath then
+      pure Nothing
+    else
+      nearestElmJsonHelp next
+  
 main :: IO ()
 main = do
-  here <- getCurrentDirectory
-  putStrLn here
+  elmJson <- nearestElmJson
+  putStrLn (show elmJson)
