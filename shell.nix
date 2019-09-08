2fd304394f39b3ada361aa1dec01465b8be2ad17
Brian Hicks <brian@brianthicks.com>
2019-09-08 06:10:30 -0500

    hindent

--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
 { nixpkgs ? import <nixpkgs> { }, compiler ? "ghc865" }:
 (import ./release.nix { inherit compiler; }).elm-swapper.env.overrideAttrs (attrs: {
-  buildInputs = attrs.buildInputs ++ [ nixpkgs.git nixpkgs.elmPackages.elm nixpkgs.cabal2nix nixpkgs.cabal-install ];
+  buildInputs = attrs.buildInputs
+    ++ [ nixpkgs.git nixpkgs.elmPackages.elm nixpkgs.cabal2nix nixpkgs.cabal-install nixpkgs.haskellPackages.hindent ];
 })
--- src/Main.hs
+++ src/Main.hs
@@ -4,23 +4,19 @@ import System.Directory
 import System.FilePath
 
 nearestElmJson :: IO (Maybe FilePath)
-nearestElmJson =
-  getCurrentDirectory >>= nearestElmJsonHelp
+nearestElmJson = getCurrentDirectory >>= nearestElmJsonHelp
 
 nearestElmJsonHelp :: FilePath -> IO (Maybe FilePath)
 nearestElmJsonHelp filepath = do
   let candidate = filepath </> "elm.json"
   exists <- doesFileExist candidate
-  if exists then
-    pure $ Just candidate
-  else
-    let next = takeDirectory filepath in
+  if exists
+    then pure $ Just candidate
+    else let next = takeDirectory filepath
+          in if next == filepath
+               then pure Nothing
+               else nearestElmJsonHelp next
 
-    if next == filepath then
-      pure Nothing
-    else
-      nearestElmJsonHelp next
-  
 main :: IO ()
 main = do
   elmJson <- nearestElmJson
