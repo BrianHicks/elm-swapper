a8bd830636825c482d8aec60ddcbcfa8da8e0874
Brian Hicks <brian@brianthicks.com>
2019-09-08 14:37:14 -0500

    sort shell deps

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 { nixpkgs ? import <nixpkgs> { }, compiler ? "ghc865" }:
 (import ./release.nix { inherit compiler; }).elm-swapper.env.overrideAttrs (attrs: {
   buildInputs = attrs.buildInputs ++ [
-    nixpkgs.git
-    nixpkgs.elmPackages.elm
-    nixpkgs.cabal2nix
-    nixpkgs.cabal-install
-    nixpkgs.haskellPackages.hindent
     nixpkgs.bench
+    nixpkgs.cabal-install
+    nixpkgs.cabal2nix
+    nixpkgs.elmPackages.elm
+    nixpkgs.git
+    nixpkgs.haskellPackages.hindent
   ];
 })
