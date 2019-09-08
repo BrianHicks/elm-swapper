e056f8ddc71eb39b23a44ef312feb57f7f6b195f
Brian Hicks <brian@brianthicks.com>
2019-09-08 14:37:02 -0500

    add bench to shell deps

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
 { nixpkgs ? import <nixpkgs> { }, compiler ? "ghc865" }:
 (import ./release.nix { inherit compiler; }).elm-swapper.env.overrideAttrs (attrs: {
-  buildInputs = attrs.buildInputs
-    ++ [ nixpkgs.git nixpkgs.elmPackages.elm nixpkgs.cabal2nix nixpkgs.cabal-install nixpkgs.haskellPackages.hindent ];
+  buildInputs = attrs.buildInputs ++ [
+    nixpkgs.git
+    nixpkgs.elmPackages.elm
+    nixpkgs.cabal2nix
+    nixpkgs.cabal-install
+    nixpkgs.haskellPackages.hindent
+    nixpkgs.bench
+  ];
 })
