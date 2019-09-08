464def86d3eb02a602a4d96aef2de4c71fb3e424
Brian Hicks <brian@brianthicks.com>
2019-09-08 05:26:21 -0500

    add cabal tool in shell

--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
 { nixpkgs ? import <nixpkgs> { }, compiler ? "ghc865" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env.overrideAttrs
-(attrs: { buildInputs = attrs.buildInputs ++ [ nixpkgs.git nixpkgs.elmPackages.elm nixpkgs.cabal2nix ]; })
+(import ./default.nix { inherit nixpkgs compiler; }).env.overrideAttrs (attrs: {
+  buildInputs = attrs.buildInputs ++ [ nixpkgs.git nixpkgs.elmPackages.elm nixpkgs.cabal2nix nixpkgs.cabal-install ];
+})
