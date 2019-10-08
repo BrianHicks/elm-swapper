4e5eb1856f107b752d252b1673bdf247a233535a
Brian Hicks <brian@brianthicks.com>
2019-10-08 13:26:37 -0500

    add ormolu

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@ let
   sources = import ./nix/sources.nix;
   niv = import sources.niv { };
   nixpkgs = import sources.nixpkgs { };
+  ormolu = import sources.ormolu { };
 in with nixpkgs;
 (import ./release.nix { inherit compiler; }).elm-swapper.env.overrideAttrs (attrs: {
-  buildInputs = attrs.buildInputs
-    ++ [ bench cabal-install cabal2nix elmPackages.elm git haskellPackages.hindent niv.niv ];
+  buildInputs = attrs.buildInputs ++ [ bench cabal-install cabal2nix elmPackages.elm git ormolu.ormolu niv.niv ];
 })
